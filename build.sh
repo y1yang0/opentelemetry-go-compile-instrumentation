d0c0555cbc290ad1f685fe72b9eb14c6451410ea
Yi Yang <1948638989@qq.com>
2025-03-03 15:26:16 +0800

    Update build.sh
    Co-authored-by: Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ go build -a -o otel
 TOOL=$(pwd)/otel
 # compile-time instrumentation via toolexec
 cd demo
-go build -a -toolexec=$TOOL -o demo .
+go build -a -work -toolexec=$TOOL -o demo .
 ./demo
