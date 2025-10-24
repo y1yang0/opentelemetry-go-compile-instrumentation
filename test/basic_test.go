c3f69a31b10d2d903b2b675199d70512f196fddf
Yi Yang <qingfeng.yy@alibaba-inc.com>
2025-10-24 15:13:16 +0800

    add doc

--- test/basic_test.go
+++ test/basic_test.go
@@ -4,42 +4,11 @@
 package test
 
 import (
-	"os/exec"
 	"path/filepath"
 	"testing"
 
-	"github.com/open-telemetry/opentelemetry-go-compile-instrumentation/tool/util"
 	"github.com/stretchr/testify/require"
 )
-
-func RunCmd(dir string, args ...string) (string, error) {
-	path := args[0]
-	args = args[1:]
-	cmd := exec.Command(path, args...)
-	cmd.Dir = dir
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return string(output), err
-	}
-	return string(output), nil
-}
-
-func RunInstrument(t *testing.T, appDir string, args ...string) {
-	binName := "otel"
-	if util.IsWindows() {
-		binName += ".exe"
-	}
-	otelPath := filepath.Join("..", "..", binName)
-	args = append([]string{otelPath}, args...)
-	out, err := RunCmd(appDir, args...)
-	require.NoError(t, err, out)
-}
-
-func RunApp(t *testing.T, dir string) string {
-	out, err := RunCmd(dir, "./"+filepath.Base(dir))
-	require.NoError(t, err, out)
-	return out
-}
 
 func TestBasic(t *testing.T) {
 	appDir := filepath.Join("..", "demo", "basic")
@@ -52,6 +21,7 @@ func TestBasic(t *testing.T) {
 		"traceID: 123, spanID: 456",
 		"[MyHook]",
 		"=setupOpenTelemetry=",
+		"RawCode",
 	}
 	for _, e := range expect {
 		require.Contains(t, output, e)
---
+++ test/infra.go
@@ -0,0 +1,42 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package test
+
+import (
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-go-compile-instrumentation/tool/util"
+	"github.com/stretchr/testify/require"
+)
+
+func RunCmd(dir string, args ...string) (string, error) {
+	path := args[0]
+	args = args[1:]
+	cmd := exec.Command(path, args...)
+	cmd.Dir = dir
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(output), err
+	}
+	return string(output), nil
+}
+
+func RunInstrument(t *testing.T, appDir string, args ...string) {
+	binName := "otel"
+	if util.IsWindows() {
+		binName += ".exe"
+	}
+	otelPath := filepath.Join("..", "..", binName)
+	args = append([]string{otelPath}, args...)
+	out, err := RunCmd(appDir, args...)
+	require.NoError(t, err, out)
+}
+
+func RunApp(t *testing.T, dir string) string {
+	out, err := RunCmd(dir, "./"+filepath.Base(dir))
+	require.NoError(t, err, out)
+	return out
+}
