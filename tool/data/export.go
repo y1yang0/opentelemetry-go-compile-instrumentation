dbbe3a1a8b33d2d898bda6800c70fbf8d64e7ed7
Yi Yang <qingfeng.yy@alibaba-inc.com>
2026-01-14 17:52:57 +0800

    fix

--- tool/data/export.go
+++ tool/data/export.go
@@ -4,29 +4,12 @@ package data
 
 import (
 	"embed"
-	"strings"
 
 	"github.com/open-telemetry/opentelemetry-go-compile-instrumentation/tool/ex"
 )
 
 //go:embed *
 var dataFs embed.FS
-
-// ListEmbedFiles lists all the files in the embedded data
-func ListEmbedFiles() ([]string, error) {
-	rules, err := dataFs.ReadDir(".")
-	if err != nil {
-		return nil, ex.Wrapf(err, "failed to read directory")
-	}
-
-	var ruleFiles []string
-	for _, rule := range rules {
-		if !rule.IsDir() && strings.HasSuffix(rule.Name(), ".yaml") {
-			ruleFiles = append(ruleFiles, rule.Name())
-		}
-	}
-	return ruleFiles, nil
-}
 
 // ReadEmbedFile reads a file from the embedded data
 func ReadEmbedFile(path string) ([]byte, error) {
